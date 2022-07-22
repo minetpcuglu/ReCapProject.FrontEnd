462003acd17fabb1fa300e55be4724a1dc78a6ae
Mine Topcuoglu <minetopcuoglu6@gmail.com>
2022-07-22 11:05:19 +0300

    22.07 car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,38 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'ReCapProject.FrontEnd';
-  car1 = {
-    carId: 1,
-    brandId: 1,
-    colorId: 1,
-    modelYear: 1999,
-    dailyPrice: 200000,
-    description: 'mine',
-  };
-  car2 = {
-    carId: 2,
-    brandId: 2,
-    colorId: 2,
-    modelYear: 2000,
-    dailyPrice: 300000,
-    description: 'Elif',
-  };
-  car3 = {
-    carId: 1,
-    brandId: 1,
-    colorId: 1,
-    modelYear: 1990,
-    dailyPrice: 500000,
-    description: 'emre',
-  };
-  car4 = {
-    carId: 1,
-    brandId: 1,
-    colorId: 1,
-    modelYear: 1998,
-    dailyPrice: 600000,
-    description: 'gönül',
-  };
 
-  cars = [this.car1, this.car2, this.car3, this.car4];
 }
